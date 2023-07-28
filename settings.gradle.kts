aec71c78a00f7643a8221c762ad577d68b0f7ae3
Albert Casanovas <caay2000@gmail.com>
2023-07-29 01:22:47 +0200

    Initial Library Project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 
-rootProject.name = "project-skeleton"
+rootProject.name = "library-kata"
 include(
     "app",
     "libs:common-arrow",
