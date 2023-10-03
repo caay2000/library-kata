1633936552e27324330aa383d90d379399e37d25
Albert Casanovas <caay2000@gmail.com>
2023-10-03 16:43:59 +0200

    - jib containers

--- build-logic/src/main/kotlin/project-application.gradle.kts
+++ build-logic/src/main/kotlin/project-application.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    // Apply the common convention plugin for shared build configuration between library and application projects.
     id("project-common")
-
-    // Apply the application plugin to add support for building a CLI application in Java.
+    id("com.google.cloud.tools.jib")
     application
 }
 
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
 }
+
+
