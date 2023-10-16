4071a4245c3d1756c36ab6cd6a74eeae7b0a6d71
Albert Casanovas <caay2000@gmail.com>
2023-10-16 23:31:19 +0200

    kontlix serialization working as expected

--- libs/common-http/src/main/kotlin/com/github/caay2000/common/http/Transfomer.kt
+++ libs/common-http/src/main/kotlin/com/github/caay2000/common/http/Transfomer.kt
@@ -7,7 +7,9 @@ interface Transfomer<I, O : JsonApiDocument> {
     fun invoke(value: I, includes: List<String> = emptyList()): O
 
     fun <T> List<String>.shouldProcess(include: RequestInclude, block: () -> List<T>): List<T> =
-        if (contains(include.toString().uppercase())) block()
-        else emptyList()
-
+        if (contains(include.toString().uppercase())) {
+            block()
+        } else {
+            emptyList()
+        }
 }
