a227081b799e14d222d262607f3de8b2d9590ec5
Albert Casanovas <caay2000@gmail.com>
2024-01-22 00:47:36 +0100

    REMOVE old event driven implementation

--- app-event-driven-old/src/main/kotlin/com/github/caay2000/librarykata/event/context/account/application/loan/search/LoanSearcher.kt
+++
@@ -1,9 +0,0 @@
-package com.github.caay2000.librarykata.event.context.account.application.loan.search
-
-import com.github.caay2000.librarykata.event.context.account.application.LoanRepository
-import com.github.caay2000.librarykata.event.context.account.domain.AccountId
-import com.github.caay2000.librarykata.event.context.account.domain.Loan
-
-class LoanSearcher(private val loanRepository: LoanRepository) {
-    fun invoke(accountId: AccountId): List<Loan> = loanRepository.searchByAccountId(accountId)
-}
